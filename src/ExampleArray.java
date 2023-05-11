5767e2685f10daf17d9452452c1ffbeb48e9ad71
shaikjanibasha1302 <shaikjanibasha.vtalent@gmail.com>
2023-05-11 15:18:17 +0530

    add two more fields

--- src/ExampleArray.java
+++ src/ExampleArray.java
@@ -2,12 +2,15 @@
 public class ExampleArray {
 
 	public static void main(String[] args) {
-		int[] marks = new int[5];
+		int[] marks = new int[7];
 		marks[0] = 98;
 		marks[1] = 99;
 		marks[2] = 97;
 		marks[3] = 96;
 		marks[4]= 93;
+		marks[5]= 93;
+		marks[6]= 93;
+
 		
 //		for(int i=0; i<marks.length; i++) {
 //			System.out.println(marks[i]);
